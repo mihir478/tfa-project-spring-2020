6eff3397a28b622f08fd8469b03bb081d042e291
Mihir478 <mihir.h.sanghavi@gmail.com>
2020-04-05 22:35:37 -0400

    get 2 columns to work

--- manage.py
+++ manage.py
@@ -1,77 +1,78 @@
 import sys
-
-import csv, sqlite3
-
-if sys.argv[1] == "import_squirrel_data":
-  path = sys.argv[2]
-  import_squirrel_data(path)
-
+import csv
+import sqlite3
 
 def import_squirrel_data(path):
 
-    columns = ["X", "Y", "Unique Squirrel ID", "Hectare", "Shift", "Date", "Hectare Squirrel Number", "Age",
-               "Primary Fur Color", "Highlight Fur Color", "Combination of Primary and Highlight Color", "Color notes",
-               "Location",
-               "Above Ground Sighter Measurement", "Specific Location", "Running", "Chasing", "Climbing", "Eating",
-               "Foraging",
-               "Other Activities", "Kuks", "Quaas", "Moans", "Tail flags", "Tail twitches", "Approaches", "Indifferent",
-               "Runs from", "Other Interactions", "Lat/Long", "Zip Codes", "Community Districts", "Borough Boundaries",
-               "City Council Districts", "Police Precincts"]
+    columns = ['X', 'Y', 'Unique Squirrel ID', 'Hectare', 'Shift', 'Date', 'Hectare Squirrel Number', 'Age',
+               'Primary Fur Color', 'Highlight Fur Color', 'Combination of Primary and Highlight Color', 'Color notes',
+               'Location',
+               'Above Ground Sighter Measurement', 'Specific Location', 'Running', 'Chasing', 'Climbing', 'Eating',
+               'Foraging',
+               'Other Activities', 'Kuks', 'Quaas', 'Moans', 'Tail flags', 'Tail twitches', 'Approaches', 'Indifferent',
+               'Runs from', 'Other Interactions', 'Lat/Long', 'Zip Codes', 'Community Districts', 'Borough Boundaries',
+               'City Council Districts', 'Police Precincts']
 
-    con = sqlite3.connect(":memory:")
+    con = sqlite3.connect(':memory:')
     cur = con.cursor()
-    commaDelimitedColumns = columns.join(".")
-    cur.execute(f"CREATE TABLE SquirrelData ({commaDelimitedColumns});")
+    cur.execute(f'CREATE TABLE SquirrelData (X, Y);')
 
-    with open(path, 'rb') as fin:
+    with open(path, 'rt') as fin:
         dr = csv.DictReader(fin)
         to_db = [
             (
-              i['Xi'],
-              i['Yi'],
-              i['UniqueSquirrelIDi'],
-              i['Hectarei'],
-              i['Shifti'],
-              i['Datei'],
-              i['HectareSquirrelNumberi'],
-              i['Agei'],
-              i['PrimaryFurColori'],
-              i['HighlightFurColori'],
-              i['CombinationofPrimaryandHighlightColori'],
-              i['Colornotesi'],
-              i['Locationi'],
-              i['AboveGroundSighterMeasurementi'],
-              i['SpecificLocationi'],
-              i['Runningi'],
-              i['Chasingi'],
-              i['Climbingi'],
-              i['Eatingi'],
-              i['Foragingi'],
-              i['OtherActivitiesi'],
-              i['Kuksi'],
-              i['Quaasi'],
-              i['Moansi'],
-              i['Tailflagsi'],
-              i['Tailtwitchesi'],
-              i['Approachesi'],
-              i['Indifferenti'],
-              i['Runsfromi'],
-              i['OtherInteractionsi'],
-              i['Lat/Longi'],
-              i['ZipCodesi'],
-              i['CommunityDistrictsi'],
-              i['BoroughBoundariesi'],
-              i['CityCouncilDistrictsi'],
-              i['PolicePrecinctsi']) for i in dr]
+                i['X'],
+                i['Y']
+                # i['UniqueSquirrelID'],
+                # i['Hectare'],
+                # i['Shift'],
+                # i['Date'],
+                # i['HectareSquirrelNumber'],
+                # i['Age'],
+                # i['PrimaryFurColor'],
+                # i['HighlightFurColor'],
+                # i['CombinationofPrimaryandHighlightColor'],
+                # i['Colornotes'],
+                # i['Location'],
+                # i['AboveGroundSighterMeasurement'],
+                # i['SpecificLocation'],
+                # i['Running'],
+                # i['Chasing'],
+                # i['Climbing'],
+                # i['Eating'],
+                # i['Foraging'],
+                # i['OtherActivities'],
+                # i['Kuks'],
+                # i['Quaas'],
+                # i['Moans'],
+                # i['Tailflags'],
+                # i['Tailtwitches'],
+                # i['Approaches'],
+                # i['Indifferent'],
+                # i['Runsfrom'],
+                # i['OtherInteractions'],
+                # i['Lat/Long'],
+                # i['ZipCodes'],
+                # i['CommunityDistricts'],
+                # i['BoroughBoundaries'],
+                # i['CityCouncilDistricts'],
+                # i['PolicePrecincts']
+            ) for i in dr]
 
-    cur.executemany(f"INSERT INTO SquirrelData ({commaDelimitedColumns}) VALUES (?, ?);", to_db)
+    cur.executemany(f'INSERT INTO SquirrelData (X, Y) VALUES (?, ?);', to_db)
 
-    cur.execute(f"SELECT * FROM SquirrelData;", to_db)
+    cur.execute(f'SELECT X, Y FROM SquirrelData;')
 
     rows = cur.fetchall()
 
     for row in rows:
-      print(row)
+        print(row)
 
     con.commit()
     con.close()
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'import_squirrel_data':
+        path = sys.argv[2]
+    import_squirrel_data(path)
