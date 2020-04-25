97714d392d5c527c760a41b632ace5eac2c49503
Mihir478 <mihir.h.sanghavi@gmail.com>
2020-04-25 19:56:18 -0400

    fix typos and format

--- squirrels/tracker/models.py
+++ squirrels/tracker/models.py
@@ -7,13 +7,13 @@ class Sighting(models.Model):
         ('pm', 'PM'),
     )
     AGE_CHOICES = (
-        ('adult','Adult'),
-        ('juvenile','Juvenile'),
+        ('adult', 'Adult'),
+        ('juvenile', 'Juvenile'),
     )
     FUR_CHOICES = (
-        ('gray','Gray'),
-        ('cinnamon','Cinnamon'),
-        ('black','Black'),
+        ('gray', 'Gray'),
+        ('cinnamon', 'Cinnamon'),
+        ('black', 'Black'),
     )
     LOCATION_CHOICES = (
         ('ground_plane', 'Ground Plane'),
@@ -21,27 +21,27 @@ class Sighting(models.Model):
     )
     x = models.FloatField('X')
     y = models.FloatField('Y')
-    unique_squirrel_id = models.ForeignKey('Squirrel')
+    unique_squirrel_id = models.CharField('Squirrel', max_length=20)
     hectare = models.CharField('Hectare', max_length=3)
-    shift = models.CharField('Shift', max_length=2,choices=SHIFT_CHOICES )
-    date = models.dateField('Date')
-    age = models.CharField('Age',max_length=50,choices=AGE_CHOICES)
-    primary_fur_color = models.CharField('Primary Fur Color',max_length=50,choices=FUR_CHOICES)
-    location = models.CharField('Location',max_length=50,choices=LOCATION_CHOICES)
-    sepcific_location = models.CharField('Specific Location',max_length=100)
-    running = models.NullBooleanField('Running')
-    chasing = models.NullBooleanField('Chasing')
-    climbing = models.NullBooleanField('Climbing')
-    eating = models.NullBooleanField('Eating')
-    foraging = models.NullBooleanField('Foraging')
+    shift = models.CharField('Shift', max_length=2, choices=SHIFT_CHOICES)
+    date = models.DateField('Date')
+    age = models.CharField('Age', max_length=50, choices=AGE_CHOICES)
+    primary_fur_color = models.CharField('Primary Fur Color', max_length=50, choices=FUR_CHOICES)
+    location = models.CharField('Location', max_length=50, choices=LOCATION_CHOICES)
+    specific_location = models.CharField('Specific Location', max_length=100)
+    running = models.BooleanField('Running')
+    chasing = models.BooleanField('Chasing')
+    climbing = models.BooleanField('Climbing')
+    eating = models.BooleanField('Eating')
+    foraging = models.BooleanField('Foraging')
     other_activities = models.CharField('Other Activities', max_length=100)
-    kuks = models.NullBooleanField('Kuks')
-    quaas = models.NullBooleanField('Quaas')
-    moans = models.NullBooleanField('Moans')
-    tail_flags = models.NullBooleanField('Tail_flags')
-    tail_twitches = models.NullBooleanField('Tail twitches')
-    approaches = models.NullBooleanField('Approaches')
-    indifferent = models.NullBooleanField('Indifferent')
-    runs_from = models.NullBooleanField('Runs from')
+    kuks = models.BooleanField('Kuks')
+    quaas = models.BooleanField('Quaas')
+    moans = models.BooleanField('Moans')
+    tail_flags = models.BooleanField('Tail flags')
+    tail_twitches = models.BooleanField('Tail twitches')
+    approaches = models.BooleanField('Approaches')
+    indifferent = models.BooleanField('Indifferent')
+    runs_from = models.BooleanField('Runs from')
 
 
