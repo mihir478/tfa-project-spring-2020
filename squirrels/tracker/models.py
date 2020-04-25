8d6478b2f4f146561b942089a9e4d97d02574b58
Yufei Tan <yt2711@columbia.edu>
2020-04-25 19:22:32 -0400

    Add variables for models

--- squirrels/tracker/models.py
+++ squirrels/tracker/models.py
@@ -3,13 +3,45 @@ from django.db import models
 # Create your models here.
 class Sighting(models.Model):
     SHIFT_CHOICES = (
-        ('am','AM'),
-        ('pm','PM'),
+        ('am', 'AM'),
+        ('pm', 'PM'),
+    )
+    AGE_CHOICES = (
+        ('adult','Adult'),
+        ('juvenile','Juvenile'),
+    )
+    FUR_CHOICES = (
+        ('gray','Gray'),
+        ('cinnamon','Cinnamon'),
+        ('black','Black'),
+    )
+    LOCATION_CHOICES = (
+        ('ground_plane', 'Ground Plane'),
+        ('above_ground', 'Above Ground'),
     )
     x = models.FloatField('X')
     y = models.FloatField('Y')
     unique_squirrel_id = models.ForeignKey('Squirrel')
     hectare = models.CharField('Hectare', max_length=3)
     shift = models.CharField('Shift', max_length=2,choices=SHIFT_CHOICES )
+    date = models.dateField('Date')
+    age = models.CharField('Age',max_length=50,choices=AGE_CHOICES)
+    primary_fur_color = models.CharField('Primary Fur Color',max_length=50,choices=FUR_CHOICES)
+    location = models.CharField('Location',max_length=50,choices=LOCATION_CHOICES)
+    sepcific_location = models.CharField('Specific Location',max_length=100)
+    running = models.NullBooleanField('Running')
+    chasing = models.NullBooleanField('Chasing')
+    climbing = models.NullBooleanField('Climbing')
+    eating = models.NullBooleanField('Eating')
+    foraging = models.NullBooleanField('Foraging')
+    other_activities = models.CharField('Other Activities', max_length=100)
+    kuks = models.NullBooleanField('Kuks')
+    quaas = models.NullBooleanField('Quaas')
+    moans = models.NullBooleanField('Moans')
+    tail_flags = models.NullBooleanField('Tail_flags')
+    tail_twitches = models.NullBooleanField('Tail twitches')
+    approaches = models.NullBooleanField('Approaches')
+    indifferent = models.NullBooleanField('Indifferent')
+    runs_from = models.NullBooleanField('Runs from')
 
-#sighting = X,Y,Unique Squirrel ID,Hectare,Shift,Date,Hectare Squirrel Number,Age,Primary Fur Color,Highlight Fur Color,Combination of Primary and Highlight Color,Color notes,Location,Above Ground Sighter Measurement,Specific Location,Running,Chasing,Climbing,Eating,Foraging,Other Activities,Kuks,Quaas,Moans,Tail flags,Tail twitches,Approaches,Indifferent,Runs from,Other Interactions,Lat/Long,Zip Codes,Community Districts,Borough Boundaries,City Council Districts,Police Precincts
+
