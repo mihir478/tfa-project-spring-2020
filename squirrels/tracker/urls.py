23c9f03643090e9d79a9f7eacd8c0bc123ee3dac
Yufei Tan <yt2711@columbia.edu>
2020-05-02 18:10:39 -0400

    Add a sighting view

--- squirrels/tracker/urls.py
+++ squirrels/tracker/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('map/', views.sightings_map),
+    path('sighting/<str:unique_squirrel_id>/', views.get_sighting)
 ]
--- squirrels/tracker/views.py
+++ squirrels/tracker/views.py
@@ -11,3 +11,10 @@ def index():
 def sightings_map(request):
     sightings = Sighting.objects.all()
     return render(request, 'tracker/map.html', {'sightings': sightings})
+
+
+def get_sighting(request, unique_squirrel_id):
+    print(unique_squirrel_id)
+    sighting = Sighting.objects.get(unique_squirrel_id=unique_squirrel_id)
+    print(sighting)
+    return render(request, 'tracker/sighting.html', {'sighting': sighting})
