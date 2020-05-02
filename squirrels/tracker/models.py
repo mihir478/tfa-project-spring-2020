03c5a23142e89defae303148d6e48cbdc520d89d
Mihir478 <mihir.h.sanghavi@gmail.com>
2020-05-02 17:19:38 -0400

    Get map to show sightings

--- squirrels/tracker/models.py
+++ squirrels/tracker/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Sighting(models.Model):
     SHIFT_CHOICES = (
         ('am', 'AM'),
@@ -10,38 +10,11 @@ class Sighting(models.Model):
         ('adult', 'Adult'),
         ('juvenile', 'Juvenile'),
     )
-    FUR_CHOICES = (
-        ('gray', 'Gray'),
-        ('cinnamon', 'Cinnamon'),
-        ('black', 'Black'),
-    )
-    LOCATION_CHOICES = (
-        ('ground_plane', 'Ground Plane'),
-        ('above_ground', 'Above Ground'),
-    )
-    x = models.FloatField('X')
-    y = models.FloatField('Y')
-    unique_squirrel_id = models.CharField('Squirrel', max_length=20)
-    hectare = models.CharField('Hectare', max_length=3)
+    longitude = models.FloatField('Longitude')
+    latitude = models.FloatField('Latitude')
+    unique_squirrel_id = models.CharField('Unique Squirrel ID', max_length=20)
     shift = models.CharField('Shift', max_length=2, choices=SHIFT_CHOICES)
     date = models.DateField('Date')
     age = models.CharField('Age', max_length=50, choices=AGE_CHOICES)
-    primary_fur_color = models.CharField('Primary Fur Color', max_length=50, choices=FUR_CHOICES)
-    location = models.CharField('Location', max_length=50, choices=LOCATION_CHOICES)
-    specific_location = models.CharField('Specific Location', max_length=100)
-    running = models.BooleanField('Running')
-    chasing = models.BooleanField('Chasing')
-    climbing = models.BooleanField('Climbing')
-    eating = models.BooleanField('Eating')
-    foraging = models.BooleanField('Foraging')
-    other_activities = models.CharField('Other Activities', max_length=100)
-    kuks = models.BooleanField('Kuks')
-    quaas = models.BooleanField('Quaas')
-    moans = models.BooleanField('Moans')
-    tail_flags = models.BooleanField('Tail flags')
-    tail_twitches = models.BooleanField('Tail twitches')
-    approaches = models.BooleanField('Approaches')
-    indifferent = models.BooleanField('Indifferent')
-    runs_from = models.BooleanField('Runs from')
 
 
--- squirrels/tracker/views.py
+++ squirrels/tracker/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return HttpResponse('Hello, BOBO!')
+from .models import Sighting
+
+
+def index():
+    return HttpResponse('Hello, User!')
+
+
+def sightings_map(request):
+    sightings = Sighting.objects.all()
+    return render(request, 'tracker/map.html', {'sightings': sightings})
