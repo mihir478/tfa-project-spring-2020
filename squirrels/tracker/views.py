2edcf6f2554e5741b69380c817ea2303d538e7ed
Mihir478 <mihir.h.sanghavi@gmail.com>
2020-05-03 19:13:09 -0400

    timeseries for sightings

--- squirrels/tracker/views.py
+++ squirrels/tracker/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Sighting
-
+import json
 
 def index():
     return HttpResponse('Hello, User!')
@@ -35,16 +35,32 @@ def stats(request):
     age_juvenile_formatted = '{:.{}f}'.format(age_juvenile, precision)
     age_missing_formatted = '{:.{}f}'.format(age_missing, precision)
 
-    color_gray = '{:.{}f}'.format(len(sightings.filter(primary_fur_color='Gray'))/count * 100,precision)
-    color_black = '{:.{}f}'.format(len(sightings.filter(primary_fur_color='Black'))/count * 100,precision)
-    color_cinnamon = '{:.{}f}'.format(len(sightings.filter(primary_fur_color='Cinnamon'))/count * 100,precision)
+    color_gray = len(sightings.filter(primary_fur_color='Gray'))/count * 100
+    color_cinnamon = len(sightings.filter(primary_fur_color='Cinnamon')) / count * 100
+    color_black = len(sightings.filter(primary_fur_color='Black')) / count * 100
+    color_missing = 100 - color_gray - color_cinnamon - color_black
+    color_gray_formatted = '{:.{}f}'.format(color_gray, precision)
+    color_cinnamon_formatted = '{:.{}f}'.format(color_cinnamon, precision)
+    color_black_formatted = '{:.{}f}'.format(color_black, precision)
+    color_missing_formatted = '{:.{}f}'.format(color_missing, precision)
+
+    date_dict = dict.fromkeys(map(lambda s: s.date.strftime('%Y-%m-%d'), sightings), 0)
+    for sighting in sightings:
+        date_str = sighting.date.strftime('%Y-%m-%d')
+        if date_dict[date_str]:
+            date_dict[date_str] += 1
+        else:
+            date_dict[date_str] = 1
+    sighting_timeseries = json.dumps(date_dict)
 
     return render(request, 'tracker/stats.html', {
         'count': count,
         'age_adult': age_adult_formatted,
         'age_juvenile': age_juvenile_formatted,
         'age_missing': age_missing_formatted,
-        'color_gray': color_gray,
-        'color_black': color_black,
-        'color_cinnamon': color_cinnamon
+        'color_gray': color_gray_formatted,
+        'color_black': color_black_formatted,
+        'color_cinnamon': color_cinnamon_formatted,
+        'color_missing': color_missing_formatted,
+        'sighting_timeseries': sighting_timeseries
     })
