eb4609ead8ede782c0e6e27796f36c81d8f55d10
Mihir478 <mihir.h.sanghavi@gmail.com>
2020-05-09 18:07:51 -0400

    remove main from apps

--- squirrels/squirrels/wsgi.py
+++ squirrels/squirrels/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'squirrels.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'squirrels.squirrels.settings')
 
 application = get_wsgi_application()
